4ddf860885a45a41b54f8accde31295468d14f3f
James Fulford <james.fulford@outlook.com>
2019-03-28 21:03:18 -0400

    WIP(app): Adding core of app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Provider } from 'react-redux';
+
+import {
+  state,
+  localization,
+} from 'is-it-shabbat-core';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import initialize from './initialization';
+import IsItShabbat from './components/IsItShabbat';
+
+const { en: { translate } } = localization;
+
+const SETUP_STATE = {
+  SETUP_NEEDED: 0,
+  LOADING: 1,
+  DONE: 2,
+};
+
+const Uninitialized = () => (
+  <>
+  <span id="is-it">-</span>
+  <span id="byline">
+    <span id="days-left">-</span>
+    <br />
+    <span id="day-or-days">{translate.endEventName.NOT_SHABBAT}</span>
+  </span>
+  </>
+);
+
+const App = () => {
+  const [initialized, setInitialized] = useState(SETUP_STATE.SETUP_NEEDED);
+
+  useEffect(() => {
+    if (initialized === SETUP_STATE.SETUP_NEEDED) {
+      setInitialized(SETUP_STATE.LOADING);
+      initialize().then(() => {
+        setInitialized(SETUP_STATE.DONE);
+      });
+    }
+  });
+
+  return (
+    <div className="container">
+      <span id="title">Is It Shabbat?</span>
+      {
+        initialized === SETUP_STATE.DONE
+          ? (
+            <Provider store={state}>
+              <IsItShabbat />
+            </Provider>
+          ) : <Uninitialized />
+      }
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/IsItShabbat.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import {
+    action,
+    localization,
+    components,
+    utilities,
+} from 'is-it-shabbat-core';
+
+const { ShabbatCheck, CountDown } = components;
+const { en: { translate } } = localization;
+const { DateTime } = utilities;
+
+
+function IsItShabbat(props) {
+    const { now, location, dispatch } = props;
+    return (
+        <ShabbatCheck now={ now } location = { location }>
+            {(period, countDownTo) => (
+                <>
+                <span id="is-it">{translate.status[period]}</span>
+                <span id="byline" >
+                        <CountDown
+                            end={countDownTo}
+                            start={now}
+                            callback={end => dispatch(action.setNow(end))}
+                        >
+                            {dur => (
+                                <>
+                                <span id="days-left">{dur.toFormat('d h:mm:ss')}</span>
+                                <br />
+                                <span id="day-or-days">{translate.endEventName[period]}</span>
+                                </>
+                            )}
+                        </CountDown>
+                </span>
+                </>
+            )
+            }
+
+        </ShabbatCheck>
+    );
+}
+IsItShabbat.propTypes = {
+    now: PropTypes.instanceOf(DateTime).isRequired,
+    location: PropTypes.shape({
+        coords: PropTypes.shape({
+            latitude: PropTypes.number,
+            longitude: PropTypes.number,
+        }),
+    }).isRequired,
+    dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(
+    state => ({
+        now: state.now,
+        location: state.location,
+    }),
+)(IsItShabbat);
