aec9c4b4afac64ca8878f6a15d0bfc1911f705a2
James Fulford <james.fulford@outlook.com>
2019-03-28 22:55:16 -0400

    Updates

---
+++ precache-manifest.250b545cc70bdfa059a6b105858eab40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e23357a26067d1a391be",
+    "url": "/is-it-shabbat-web/static/js/runtime~main.10d77ff3.js"
+  },
+  {
+    "revision": "494d61b714fa143b6fbc",
+    "url": "/is-it-shabbat-web/static/js/main.dce2797f.chunk.js"
+  },
+  {
+    "revision": "b05fae8401ad31c26fdf",
+    "url": "/is-it-shabbat-web/static/js/2.947d9f26.chunk.js"
+  },
+  {
+    "revision": "494d61b714fa143b6fbc",
+    "url": "/is-it-shabbat-web/static/css/main.6cf48c2d.chunk.css"
+  },
+  {
+    "revision": "81abd3439b951280c3b882718f132e64",
+    "url": "/is-it-shabbat-web/index.html"
+  }
+];
