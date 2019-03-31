5a8e0e07633d8e2e041e18525f6ace5996237bc3
James Fulford <james.fulford@outlook.com>
2019-03-31 15:31:50 -0400

    Updates

---
+++ precache-manifest.740d627ced431de933c06eb1802e6b92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "10a27e00ae63b0995349",
+    "url": "/static/js/main.c941b510.chunk.js"
+  },
+  {
+    "revision": "b05fae8401ad31c26fdf",
+    "url": "/static/js/2.947d9f26.chunk.js"
+  },
+  {
+    "revision": "10a27e00ae63b0995349",
+    "url": "/static/css/main.6cf48c2d.chunk.css"
+  },
+  {
+    "revision": "6caba388fe825a85a99faeaf2a97c6d3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/is-it-shabbat-web/precache-manifest.250b545cc70bdfa059a6b105858eab40.js"
+  "/precache-manifest.740d627ced431de933c06eb1802e6b92.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/is-it-shabbat-web/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
